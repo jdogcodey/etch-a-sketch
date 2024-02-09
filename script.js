edc987c718c17aa022d1e739ec924e5b3f43ee7b
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-02-09 12:27:55 +0000

    Made 16x16 into a grid

--- script.js
+++ script.js
@@ -10,13 +10,12 @@ for (i = 1; i <= 16; i++) {
     console.log(i)
     xContainerDivs[i] = document.createElement('div');
     xContainerDivs[i].id = 'container' + i;
-    xContainerDivs[i].class = 'container';
-    xContainerDivs[i].textContent = `This is Div ${i}`
+    xContainerDivs[i].classList.add('container');
     container.appendChild(xContainerDivs[i]);
     for (j = 1; j <= 16; j++) {
         yBoxDivs[j] = document.createElement('div');
         yBoxDivs[j].id = 'box' + j;
-        yBoxDivs[j].class = 'box';
+        yBoxDivs[j].classList.add('box')    ;
         yBoxDivs[j].textContent = `This is box ${j}`;
         xContainerDivs[i].appendChild(yBoxDivs[j]);
     }
