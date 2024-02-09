4bf5e5011afe963d80f01d5b54c856945cd29b11
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-02-09 17:02:49 +0000

    Background colour updates with mouseover
    Took me almost the whole day to debug this one.
    
    Turns out the issue was that I needed to wait
    for the page to load before it started trying
    to change the background colour.
    
    Got there in the end.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const container = document.querySelector(`#container`);
 
 i = 1;
-j = 0;
+j = 1;
+k = 1;
+l = 1;
 
 const xContainerDivs = [];
 const yBoxDivs = [];
@@ -14,9 +16,19 @@ for (i = 1; i <= 16; i++) {
     container.appendChild(xContainerDivs[i]);
     for (j = 1; j <= 16; j++) {
         yBoxDivs[j] = document.createElement('div');
-        yBoxDivs[j].id = 'box' + j;
-        yBoxDivs[j].classList.add('box')    ;
-        yBoxDivs[j].textContent = `This is box ${j}`;
+        yBoxDivs[j].id = 'box' + j + 'container' + i;
+        yBoxDivs[j].classList.add('box');
         xContainerDivs[i].appendChild(yBoxDivs[j]);
+        };
     }
-}
+
+window.addEventListener("DOMContentLoaded", function() {
+    for (k = 1; k <= 16; k++) {
+        for (l = 1; l <= 16; l++) {
+        document.getElementById(`box${k}container${l}`).addEventListener('mouseover', function () {
+            console.log(this.id);
+            this.style.backgroundColor = 'red';
+        });
+        };
+    };
+});
