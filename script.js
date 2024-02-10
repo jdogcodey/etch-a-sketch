f7a6ee4b21520bc54352294eb0db890a0ff649b9
Jonathan Dawson <Jonathan@Jonathans-MacBook.local>
2024-02-10 02:48:31 +0000

    Minor formatting differences

--- script.js
+++ script.js
@@ -28,43 +28,43 @@ function resetter() {
 }
 
 const resetButton = document.createElement('button');
-    resetButton.textContent = 'Reset';
-    resetButton.addEventListener('click', resetter);
+resetButton.textContent = 'Reset';
+resetButton.addEventListener('click', resetter);
     resetButton.classList.add('button');
 div1.appendChild(resetButton);
 
 let playRound 
 (playRound = function playAgain() {
-for (i = 1; i <= userChosenNumber; i++) {
+    for (i = 1; i <= userChosenNumber; i++) {
     // console.log(i)
-    xContainerDivs[i] = document.createElement('div');
-    xContainerDivs[i].id = 'container' + i;
-    xContainerDivs[i].classList.add('container');
-    container.appendChild(xContainerDivs[i]);
-    for (j = 1; j <= userChosenNumber; j++) {
+        xContainerDivs[i] = document.createElement('div');
+        xContainerDivs[i].id = 'container' + i;
+        xContainerDivs[i].classList.add('container');
+        container.appendChild(xContainerDivs[i]);
+        for (j = 1; j <= userChosenNumber; j++) {
        // console.log(`no of boxes` + j)
-        yBoxDivs[j] = document.createElement('div');
-        yBoxDivs[j].id = 'box' + j + 'container' + i;
-        yBoxDivs[j].classList.add('box');
-        xContainerDivs[i].appendChild(yBoxDivs[j]);
+            yBoxDivs[j] = document.createElement('div');
+            yBoxDivs[j].id = 'box' + j + 'container' + i;
+            yBoxDivs[j].classList.add('box');
+            xContainerDivs[i].appendChild(yBoxDivs[j]);
         };
     }
 })();
 
 window.addEventListener("DOMContentLoaded", function () {
-        let makeItRed
-        (makeItRed = function makingItRed() {
+    let makeItRed
+    (makeItRed = function makingItRed() {
         for (k = 1; k <= userChosenNumber; k++) {
         // console.log(`no of k` + k)
-        for (l = 1; l <= userChosenNumber; l++) {
-        document.getElementById(`box${k}container${l}`).addEventListener('mouseenter', function () {
-            // console.log(this.id);
-            let color = random_rgba();
-            this.style.backgroundColor = color;
-        });
+            for (l = 1; l <= userChosenNumber; l++) {
+                document.getElementById(`box${k}container${l}`).addEventListener('mouseenter', function () {
+                // console.log(this.id);
+                let color = random_rgba();
+                this.style.backgroundColor = color;
+                });
+            };
         };
-        };
-        })();
+    })();
     
     const noOfBoxButton = document.createElement('button');
     noOfBoxButton.textContent = 'Change pixel size';
